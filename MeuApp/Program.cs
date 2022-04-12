20d5d7aa4e96eb7e2d880cdeee282601c2c6eb6e
Paulo Spiguel <paulospil@hotmail.com>
2022-04-12 11:19:21 +0100

    added TextEditor

---
+++ MeuApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+            var text = "Testing debbug";
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ TextEditor/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Create new file");
+            Console.WriteLine("0 - Exit");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option){
+                case 0: Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+    
+        static void Open(){}
+
+        static void Edit(){
+            Console.Clear();
+            Console.WriteLine("Enter your text below (ESC fot exit)");
+            Console.WriteLine("------------------------------------");
+
+            string text = "";
+
+            do{
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+    
+        static void Save(string text){
+            Console.Clear();
+            Console.WriteLine("Where do you want to save your file?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path)){
+                file.Write(text);
+            }
+
+            Console.WriteLine("File saved successfully!");
+            Console.WriteLine($"Path: {path}");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
